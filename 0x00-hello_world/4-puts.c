e43c2e3302b316019424adbdb1e74fded260b885
Casadah <sadahcalebgmail.com>
2022-12-08 05:12:36 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+
+}
