eedd8f117ec5340c2805d9e310ad6bfdd3b59a39
Casadah <sadahcalebgmail.com>
2022-12-08 05:55:09 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-	printf("Size of char: %i byte(s)\n", sizeof(char));
-	printf("Size of char: %i byte(s)\n", sizeof(char));
-	printf("Size of char: %i byte(s)\n", sizeof(char));
-	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
 	return (0);
 }
