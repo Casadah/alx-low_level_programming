f82b4e5fd497795484b63f1e6b338ee5f5d4f40b
Casadah <sadahcalebgmail.com>
2022-12-08 05:52:52 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of char: %i byte(s)\n", sizeof(char));
+	return (0);
+}
