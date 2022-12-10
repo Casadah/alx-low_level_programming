bfaac8faff833c920a3ccaf63d2624896c9d6e79
Casadah <sadahcalebgmail.com>
2022-12-10 14:05:04 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c < = 'Z'; c++)
+	for (c = 'a'; c <= 'Z'; c++)
 		putchar(c);
-	for (c = 'A'; c < = 'Z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
 	return (0);
 }
