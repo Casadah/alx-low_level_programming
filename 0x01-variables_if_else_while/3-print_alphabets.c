a40ccbc710adc43dfd31216f2eabe1874769be79
Casadah <sadahcalebgmail.com>
2022-12-10 14:01:08 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < = 'Z'; c++)
+		putchar(c);
+	for (c = 'A'; c < = 'Z'; c++)
+		putchar(c);
+	return (0);
+}
