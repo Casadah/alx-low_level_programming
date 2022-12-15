83954049c696f9fcc3177eba472babadd813e380
Casadah <sadahcalebgmail.com>
2022-12-14 23:58:59 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lower case
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	cha la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n')
+
+	return (0);
+}
