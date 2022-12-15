f1f1b51722909c6af87a79e976e536a5b2649261
Casadah <sadahcalebgmail.com>
2022-12-15 00:59:54 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of integer
+ * @t: The interger to be computed
+ * Return: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
