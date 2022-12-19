0b15f7fab2de3d0a7957874b4af3471249e7e0ca
Casadah <sadahcalebgmail.com>
2022-12-19 02:02:25 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0 - 9
+ * @n: number
+ */
+void print_numbers(void)
+{
+	for (n = 0; n <= 9; n++)
+		_putchar(n);
+	_putchar('\n');
+}
